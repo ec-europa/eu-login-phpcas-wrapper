d41c2c1d7e86937aa0e966c15ad5e7b5e0ed59bd
adrian.marina <Adrian.MARINA@ext.ec.europa.eu>
2018-03-28 14:33:25 +0200

    added index with existing test actions

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="EU LOGIN PHP Client Wrapper">
+    <meta name="author" content="DIGIT.D.1.003">
+
+    <title>EU LOGIN PHP Client Wrapper</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+<div class="container-fluid">
+    <h2>Test Actions<br></h2>
+    <div><a href="/test.php">Login</a></div>
+    <div><a href="/test.php?action=logout">Logout</a></div>
+
+</div>
+</body>
+</html>
